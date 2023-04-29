ac04fccefc16a5c1b57c79bed27872f3e841f5d9
Raymond Turla <raymond.turla@paynamics.net>
2023-04-29 19:13:16 +0800

    Seperate contract deployment scripts into files

---
+++ scripts/Lock.ts
@@ -0,0 +1,17 @@
+import { ethers } from "hardhat";
+
+export async function deployLock() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const unlockTime = currentTimestampInSeconds + 60;
+
+  const lockedAmount = ethers.utils.parseEther("0.001");
+
+  const Lock = await ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+  await lock.deployed();
+
+  console.log(
+    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  );
+}
