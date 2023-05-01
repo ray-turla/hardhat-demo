4fa8cd46d8968d0e59e0bbf4c3cb2150b1c413a4
Raymond Turla <raymond.turla@paynamics.net>
2023-05-01 12:22:10 +0800

    Add utilities file

---
+++ utils.ts
@@ -0,0 +1,19 @@
+
+type environment = "production" | "development" | "local"
+
+function getDefaultNetwork(env: environment) {
+  let network = "hardhat"
+  switch(env) {
+    case "production":
+      network = "";
+      break;
+    case "development":
+      network = "sepolia";
+      break;
+    default:
+      break;
+  }
+  return network;
+}
+
+export { getDefaultNetwork };
